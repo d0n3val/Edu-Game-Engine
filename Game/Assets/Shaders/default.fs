bf832c4933a90ef501afd6b48ac467816a81f22d
ilgenio <krlosfuentes@gmail.com>
2019-01-17 09:43:32 +0100

    tests different roughness

--- Game/Assets/Shaders/default.fs
+++ Game/Assets/Shaders/default.fs
@@ -136,13 +136,6 @@ layout(index=3) subroutine(GetFresnel) vec3 get_no_fresnel(vec3 dir0, vec3 dir1,
     return f0;
 }
 
-vec3 get_fresnel_schlick_roughness(vec3 dir0, vec3 dir1, const vec3 f0, float roughness)
-{
-    float cos_theta = max(dot(dir0, dir1), 0.0); 
-
-    return f0+(1-f0)*pow(1.0-cos_theta, 5.0);
-}
-
 float sqr(float a)
 {
     return a*a;
@@ -274,25 +267,29 @@ vec4 lighting(const vec3 pos, const vec3 normal, float normal_len, const vec2 uv
     //color += diffuse_color.rgb*(lights.ambient.color*occlusion_color*material.k_ambient);
     color += emissive_color;
 
+    smoothness = mat.smoothness;
     float roughness = 1.0-smoothness;
-    vec3 F  = get_fresnel_schlick(view_dir, normal, specular_color.rgb); // https://seblagarde.wordpress.com/2011/08/17/hello-world/ uses roughness
-    vec3 kD = 1.0 - F;
-    vec3 normal2 = normal;
-    normal2.x = -normal2.x;
+    roughness = roughness*0.8;
 
-    vec3 irradiance = texture(irradiance_map, normal).rgb;
+    vec3 F  = get_fresnel_schlick(view_dir, normal, specular_color.rgb); 
+    vec3 kD = 1.0 - F;
+
+    vec3 normal2 = normal;
+
+    //normal2.x = -normal2.x;
+
+    vec3 irradiance = texture(irradiance_map, normal2).rgb;
     vec3 diffuse    = irradiance * diffuse_color.rgb;
 
-    const float MAX_REFLECTION_LOD = 10.0;
+    const float MAX_REFLECTION_LOD = 9.0;
 
     vec3 R = reflect(-view_dir, normal);   
 
-    R.x = -R.x;
+    //R.x = -R.x;
 
-    F = specular_color.rgb;
     vec3 prefilter_color = textureLod(prefilter_map, R,  roughness*MAX_REFLECTION_LOD).rgb;   
     vec3 env_BRDF  = texture(brdf_map, vec2(max(dot(normal, view_dir), 0.0), roughness)).rgb;
-    vec3 specular = prefilter_color * (F * env_BRDF.x + env_BRDF.y);
+    vec3 specular = prefilter_color * (specular_color.rgb * env_BRDF.x + env_BRDF.y);
     vec3 ambient = (kD * diffuse + specular) * occlusion_color; 
 
     color += ambient;
