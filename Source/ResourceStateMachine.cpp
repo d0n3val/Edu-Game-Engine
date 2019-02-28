904bf82e3e3376731cf04861edca7dda42fd746a
ilgenio <krlosfuentes@gmail.com>
2019-02-28 08:44:42 +0100

    release from memory

--- Source/ResourceStateMachine.cpp
+++ Source/ResourceStateMachine.cpp
@@ -117,6 +117,10 @@ void ResourceStateMachine::ReleaseFromMemory()
 			res->Release();
 		}
 	}
+
+    clips.clear();
+    nodes.clear();
+    transitions.clear();
 }
 
 // ---------------------------------------------------------
